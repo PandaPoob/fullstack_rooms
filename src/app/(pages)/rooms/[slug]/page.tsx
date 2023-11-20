bc8eef593c58a4de826bf7a079bd60243837020a
Freja <frej400d@stud.kea.dk>
2023-11-20 13:31:31 +0100

    added put request

--- src/app/(pages)/rooms/[slug]/page.tsx
+++ src/app/(pages)/rooms/[slug]/page.tsx
@@ -3,6 +3,22 @@ import { db } from "@/lib/prisma-client";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import RoomView from "@/app/_views/Room";
+import { Participant } from "@prisma/client";
+
+async function updateVisitedAt(participant: Participant) {
+  try {
+    await db.participant.update({
+      where: {
+        id: participant.id,
+      },
+      data: {
+        visited_at: new Date().toISOString(),
+      },
+    });
+  } catch (error) {
+    console.error("Error updating visited_at:", error);
+  }
+}
 
 async function getData(params: { slug: string }) {
   const session = await getServerSession(authOptions);
@@ -26,6 +42,19 @@ async function getData(params: { slug: string }) {
     if (!room) {
       redirect("/error");
     }
+
+    const participant = await db.participant.findFirst({
+      where: {
+        room_id: params.slug as string,
+        user_id: session.user.id as string,
+      },
+    });
+
+    if (participant) {
+      // Call the function to update visited_at
+      await updateVisitedAt(participant);
+    }
+
     const data = {
       room,
       session,
--- src/app/(pages)/rooms/page.tsx
+++ src/app/(pages)/rooms/page.tsx
@@ -27,23 +27,40 @@ async function getData(props: Session) {
         participants: {
           select: {
             visited_at: true,
+            user_id: true,
           },
         },
       },
     });
 
-    const sorted_rooms = rooms.sort((a, b) => {
-      const visitedAtA = a.participants?.[0]?.visited_at;
-      const visitedAtB = b.participants?.[0]?.visited_at;
+    const sortedRooms = rooms.slice().sort((a, b) => {
+      const participantA = a.participants.find(
+        (participant) => participant.user_id === props.user.id
+      );
+
+      const participantB = b.participants.find(
+        (participant) => participant.user_id === props.user.id
+      );
+
+      const visitedAtA = participantA?.visited_at
+        ? new Date(participantA.visited_at)
+        : null;
+      const visitedAtB = participantB?.visited_at
+        ? new Date(participantB.visited_at)
+        : null;
 
       if (visitedAtA && visitedAtB) {
-        return new Date(visitedAtB).getTime() - new Date(visitedAtA).getTime();
+        return visitedAtB.getTime() - visitedAtA.getTime();
+      } else if (visitedAtA) {
+        return -1;
+      } else if (visitedAtB) {
+        return 1;
       } else {
-        return 0; // Handle cases where visited_at is missing
+        return 0;
       }
     });
     return {
-      userRooms: sorted_rooms as Room[],
+      userRooms: sortedRooms as Room[],
     };
   } catch (error) {
     console.error("An error occurred fetching rooms", error);
@@ -54,6 +71,7 @@ async function RoomsPage() {
   //Validate and getSession
   const session = await requireAuthentication(authOptions);
   const data = await getData(session);
+  //console.log(data?.userRooms);
 
   return data && <Rooms data={data} sessionUser={session.user} />;
 }
