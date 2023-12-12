5b84a0a25ec9215bbb18173b9b4f516e4f00a64e
“clar9309” <clar9309@stud.kea.dk>
2023-12-12 22:21:42 +0100

    note list responsive

--- src/app/_views/Notes/NoteList.tsx
+++ src/app/_views/Notes/NoteList.tsx
@@ -10,7 +10,7 @@ interface NoteListProps {
 
 function NoteList(props: NoteListProps) {
   return (
-    <div className="py-7 grid grid-cols-4 gap-5 justify-center items-center">
+    <div className="py-7 grid md:grid-cols-4 gap-5 justify-center items-center">
       {props.notes.map((note: NoteItem) => (
         <Link key={note.id} href={`/rooms/${props.room_id}/notes/${note.id}`}>
           <NoteCard
